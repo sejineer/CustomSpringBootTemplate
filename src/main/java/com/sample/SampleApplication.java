b55500f2d32b220ce019aa910686292f5a726d6b
RealWorldd <jinpark99@naver.com>
2022-11-16 09:21:24 +0900

    V1

--- src/main/java/com/sample/SampleApplication.java
+++ src/main/java/com/sample/SampleApplication.java
@@ -1,13 +1,14 @@
 package com.sample;
 
+import com.sample.global.config.YamlPropertySourceFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = {"classpath:database/application-database.yml"})
-@PropertySource(value = { "classpath:oauth2/application-oauth2.yml" })
-@PropertySource(value = { "classpath:swagger/application-springdoc.yml" })
+@PropertySource(value = {"classpath:database/application-database.yml"}, factory = YamlPropertySourceFactory.class)
+@PropertySource(value = { "classpath:oauth2/application-oauth2.yml" }, factory = YamlPropertySourceFactory.class)
+@PropertySource(value = { "classpath:swagger/application-springdoc.yml" }, factory = YamlPropertySourceFactory.class)
 public class SampleApplication {
 
 	public static void main(String[] args) {
